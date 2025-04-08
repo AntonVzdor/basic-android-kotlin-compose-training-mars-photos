5f8c1d6b61c7be40ca806a306af2f8a61936bf8c
AntonVzdor <135133877+AntonVzdor@users.noreply.github.com>
2025-04-08 15:16:54 +0300

    4. Создание контейнера приложения

--- app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -1,4 +1,14 @@
 package com.example.marsphotos.data
 
+import com.example.marsphotos.model.MarsPhoto
+import com.example.marsphotos.network.MarsApi
+
 interface MarsPhotosRepository {
+    suspend fun getMarsPhoto(): List<MarsPhoto>
+}
+
+class NetworkMarsPhotosRepository(): MarsPhotosRepository{
+    override suspend fun getMarsPhoto(): List<MarsPhoto> {
+        return MarsApi.retrofitService.getPhotos()
+    }
 }
